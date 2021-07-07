0b7969434e8ac72c4bffc57e20f6136648fe1d2d
Julian Mahecha <jmahecha@hugeinc.com>
2021-07-06 20:12:20 -0500

    add dependency to clear build folder

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,11 @@
 import resolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
+import del from 'rollup-plugin-delete';
 
 export default [
   {
     input: 'index.ts',
-    plugins: [typescript(), resolve()],
+    plugins: [del({ targets: 'build/*' }), typescript(), resolve()],
     treeshake: false,
     output: [
       {
